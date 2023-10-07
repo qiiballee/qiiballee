db049859da1704b6bdb7270d4d7826c30648dac4
qiiballee <143315864+qiiballee@users.noreply.github.com>
2023-10-07 15:02:32 +0700

    Update kalkulator.js

--- kalkulator.js
+++ kalkulator.js
@@ -2,7 +2,7 @@ let displayValue = '0';
 
 function updateDisplay() {
     const display = document.getElementById('display');
-    display.textContent = displayValue;
+    display.value = displayValue;
 }
 
 function appendToDisplay(val) {
@@ -25,6 +25,26 @@ function calculateResult() {
     } catch (error) {
         displayValue = 'Error';
     }
+    updateDisplay();
+}
+
+function calculateSquareRoot() {
+    displayValue = Math.sqrt(parseFloat(displayValue)).toString();
+    updateDisplay();
+}
+
+function calculateSquare() {
+    displayValue = (parseFloat(displayValue) * parseFloat(displayValue)).toString();
+    updateDisplay();
+}
+
+function calculateInverse() {
+    displayValue = (1 / parseFloat(displayValue)).toString();
+    updateDisplay();
+}
+
+function calculatePi() {
+    displayValue = Math.PI.toString();
     updateDisplay();
 }
 
